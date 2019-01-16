c0194f2d42c29e0dd99c8a4a6235073dc8b4b15e
inpirncurse <a01204805@itesm.mx>
2019-01-16 12:46:27 -0600

    ready

---
+++ test.py
@@ -0,0 +1,13 @@
+import fileinput
+
+def main():
+
+    ans = 0
+
+    for line in fileinput.input():
+        if line.strip():
+            number = float(line)
+            ans = number + ans
+    #print(ans)
+if __name__ == '__main__':
+    main()
